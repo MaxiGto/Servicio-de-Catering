6ca484106d0160e515610be7ead2e59f3692b7f8
Maxi <maxi.gatto96@gmail.com>
2021-07-05 23:12:28 -0300

    [FT] SiteHeader Creado

---
+++ html/SiteHeader.php
@@ -0,0 +1,6 @@
+<header class="site-header">
+        <div class="contenedor-header">
+            <a href="principal" id="header-title">Catering Essen</a>
+            <a href="logout" id="header-logout">Salir</a>
+        </div>
+</header>
