28792d13c69529a0b3cfab57cc118dfb5736ab84
Maxi <maxi.gatto96@gmail.com>
2022-01-15 22:26:19 -0300

    [FT] Cambios en Logout

--- controllers/cerrarSesion.php
+++ controllers/cerrarSesion.php
@@ -4,13 +4,16 @@ require '../fw/fw.php';
 
 require '../views/Logout.php';
 
-unset($_SESSION['auth']);
-unset($_SESSION['id_usuario']);
-unset($_SESSION['usuario']);
-unset($_SESSION['rol']);
-unset($_SESSION['id_cliente']);
-unset($_SESSION['menus']);
-unset($_SESSION['servicios']);
+// unset($_SESSION['auth']);
+// unset($_SESSION['id_usuario']);
+// unset($_SESSION['usuario']);
+// unset($_SESSION['rol']);
+// unset($_SESSION['id_cliente']);
+// unset($_SESSION['menus']);
+// unset($_SESSION['servicios']);
+// unset($_SESSION['evento']);
+// unset($_SESSION['legajo']);
+session_destroy();
 
 $v = new Logout();
 $v->render();
