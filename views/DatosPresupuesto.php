ececc71a9fa5be2b98781d4666fe38676bd8994d
Maxi <maxi.gatto96@gmail.com>
2022-01-26 22:58:19 -0300

    [FT] Horas adcionales en detalle presupuesto

--- views/DatosPresupuesto.php
+++ views/DatosPresupuesto.php
@@ -9,6 +9,8 @@ class DatosPresupuesto extends View{
     public $tieneServicios = false;
     public $servicios;
     public $totalServicios = 0;
-    
+    public $tieneHorasAd = false;
+    public $cantidadHoras = 0;
+    public $precioHora = 0;
 
 }
